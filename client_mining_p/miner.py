6147adf086aaa9dc8fea1f77c9b68885742eeb36
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-07-10 18:37:14 -0400

    client mining - removed unnecessary prints

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,13 +13,6 @@ def valid_proof(last_proof, proof):
 
 
 def proof_of_work(last_proof):
-    """
-    Simple Proof of Work Algorithm
-    - Find a number p' such that hash(pp') contains 4 leading
-    zeroes, where p is the previous p'
-    - p is the previous proof, and p' is the new proof
-    """
-
     proof = 0
     while valid_proof(last_proof, proof) is False:
         proof += 1
@@ -41,7 +34,6 @@ if __name__ == '__main__':
         last_proof = requests.get(f'{node}/last_proof').json()['proof']
         print("Mining started!")
         new_proof = proof_of_work(last_proof)
-        print(new_proof, last_proof)
         # TODO: When found, POST it to the server {"proof": new_proof}
         res = requests.post(f'{node}/mine', json={"proof": new_proof})
         # TODO: If the server responds with 'New Block Forged'
@@ -50,8 +42,8 @@ if __name__ == '__main__':
             coins_mined += 1
             print("Coins mined:", coins_mined)
             start_time = time.time()
-        # else:
-            # print(res.json()['message'])
+        else:
+            print(res.json()['message'])
 
             # add 1 to the number of coins mined and print it.  Otherwise,
             # print the message from the server.
